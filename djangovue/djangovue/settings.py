2aae0f2eaa2bb2cd43ed87cdc03389e5b4f56b01
Suhrid Mathur <suhrid.mathur@industrybuying.com>
2020-10-01 11:50:16 +0530

    PyCache Removed

--- djangovue/djangovue/settings.py
+++ djangovue/djangovue/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_VUE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'djangovue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangovuedb',
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT', 5432),
+        'USER': 'suhrid',
+        'PASSWORD': 'suhrid',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
